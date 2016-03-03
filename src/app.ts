cc3634739e54564b157fa54652b042222274a708
Matthew Danskey <mdanskey@gmail.com>
2016-03-02 21:03:23 -0800

    add first generated tone via Web Audio API

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
 document.body.innerHTML = `<h1> Hello! Seeing this means everything is working as expected. </h1>`;
+
+let audioContext = new AudioContext();
+let oscillator = audioContext.createOscillator();
+let gainNode = audioContext.createGain();
+
+oscillator.connect(gainNode);
+gainNode.connect(audioContext.destination);
+
+oscillator.type = 'square';
+oscillator.frequency.value = 140;
+oscillator.detune.value = 10;
+oscillator.start(0);
+
+gainNode.gain.value = 0.1;
+
+setTimeout(() => gainNode.disconnect(), 1000);
