82cd8982abc96c59419d75614146163568c132f5
Matthew Danskey <mdanskey@gmail.com>
2016-03-03 21:27:04 -0800

    add interactive tone creation (limited to Hz/duration)

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,22 @@
-document.body.innerHTML = `<h1> Hello! Seeing this means everything is working as expected. </h1>`;
-
 let audioContext = new AudioContext();
-let oscillator = audioContext.createOscillator();
 let gainNode = audioContext.createGain();
 
-oscillator.connect(gainNode);
 gainNode.connect(audioContext.destination);
+gainNode.gain.value = 0.3;
 
-oscillator.type = 'square';
-oscillator.frequency.value = 140;
-oscillator.detune.value = 10;
-oscillator.start(0);
+function warble(context: AudioContext, frequency: number, duration: number) {
+    let oscillator = audioContext.createOscillator();
+    oscillator.type = 'triangle';
+    oscillator.connect(gainNode);
+    oscillator.frequency.value = frequency;
+    oscillator.start(0);    
+    oscillator.stop(audioContext.currentTime + duration);
+}
 
-gainNode.gain.value = 0.1;
+warble(audioContext, 120, 1);
 
-setTimeout(() => gainNode.disconnect(), 1000);
+let hertz = <HTMLInputElement>document.getElementById('hertz');
+let duration = <HTMLInputElement>document.getElementById('duration');
+let soundButton = document.getElementById('makeSound');
+
+soundButton.onclick = () => warble(audioContext, Number(hertz.value), Number(duration.value));
